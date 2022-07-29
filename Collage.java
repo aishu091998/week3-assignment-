85dcc94877bf823e2b2a15665739f593f61cc957
aishu091998 <109001941+aishu091998@users.noreply.github.com>
2022-07-29 19:10:06 +0100

    Add files via upload

---
+++ Collage.java
@@ -0,0 +1,15 @@
+package org.collage;
+
+public class Collage {
+	    public void collageName() {
+	    	System.out.println("Pondicherry University");
+	    }
+         
+	    public void collageCode() {
+	    	System.out.println("23345");
+	    }
+	    public void collageRank() {
+	    	System.out.println("5th");
+	    }
+}
+
---
+++ Department.java
@@ -0,0 +1,8 @@
+package org.department;
+
+public class Department extends org.collage.Collage{
+	             public void deptName() {
+	            	 System.out.println("B.A");
+	             }
+
+}
